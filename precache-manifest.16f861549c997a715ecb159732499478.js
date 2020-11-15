4a51eba052b52bf9ed649be1bd336456ab8d1a2c
hakgizzang <choonggi242naver.com>
2020-11-15 16:02:36 +0900

    Updates

---
+++ precache-manifest.16f861549c997a715ecb159732499478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f7bddc3b3554fe24beef477c934a10",
+    "url": "/hakizzang/index.html"
+  },
+  {
+    "revision": "1fd2e4f49b9a01521414",
+    "url": "/hakizzang/static/css/main.d9b99022.chunk.css"
+  },
+  {
+    "revision": "ca1290ec655463a20b0e",
+    "url": "/hakizzang/static/js/2.ad030fd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hakizzang/static/js/2.ad030fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd2e4f49b9a01521414",
+    "url": "/hakizzang/static/js/main.1e183c74.chunk.js"
+  },
+  {
+    "revision": "a880b6506e3aad30955f",
+    "url": "/hakizzang/static/js/runtime-main.69513cdb.js"
+  }
+]);
